48511a37c7299c7f7081db262f3de297526400b9
Rehab <rehab@pop-os.localdomain>
2020-02-19 10:56:25 +0200

    sending request from frontend (script.js) for backend (app.js) #4

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const route = require('./route');
+//const route = require('./route');
 const path =  require('path');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(route);
+app.post('/search' , (req , res , next) => {
+  const movieName = req.body.name;
+  console.log(movieName,1111111111);
+  
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.key}&query=${movieName}`)
+  .then(result => result.json())
+  .then(result => res.json(result))
+  .catch(console.error);
+})
+// app.use(route);
 
 module.exports = app;
-
-
-
-
-
-// const port = process.env.PORT || 3000;
-// app.listen(3000, () => {
-//   console.log(`http://localhost:${port}`);
-// });
