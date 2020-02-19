00746dfc658be8f367fc5672a86fe356a5868a85
Rehab <rehab@pop-os.localdomain>
2020-02-19 09:18:52 +0200

    edit app relates #4

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const route = require('./route');
+const path =  require('path');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(route);
 
-const port = process.env.PORT || 3000;
-app.listen(3000, () => {
-  console.log(`http://localhost:${port}`);
-});
+module.exports = app;
+
+
+
+
+
+// const port = process.env.PORT || 3000;
+// app.listen(3000, () => {
+//   console.log(`http://localhost:${port}`);
+// });
