986263a961151945428dddbff9c6524a698ab1c7
Rehab <rehab@pop-os.localdomain>
2020-02-19 14:14:49 +0200

    view movie details relates #18

--- server/app.js
+++ server/app.js
@@ -9,15 +9,23 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.post('/search' , (req , res , next) => {
+
+app.post('/search' , (req , res) => {
   const movieName = req.body.name;
-  console.log(movieName,1111111111);
-  
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.key}&query=${movieName}`)
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MY_KEY}&query=${movieName}`)
   .then(result => result.json())
   .then(result => res.json(result))
   .catch(console.error);
 })
+
+app.get('/movies' , (req , res) => {
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MY_KEY}&language=en-US&page=1
+  `)
+  .then(result => result.json())
+  .then(result => res.json(result))
+  .catch(console.error);
+})
+
 // app.use(route);
 
 module.exports = app;
